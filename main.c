07e1bfb3cce547905b6d349db1401bf9287a1225
Felipe <santospinheiro627@gmail.com>
2022-08-23 23:20:17 -0300

    big

--- main.c
+++ main.c
@@ -3,7 +3,11 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	printf("OLA MUNDO!");
+int main() {
+	
+	
+	printf("HELLO, WORLD!\n");
+	printf("HELLO, WORLD!\n");
+	system("pause");
 	return 0;
-	}
+}
